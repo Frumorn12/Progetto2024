e1c84e31ee7e7f423d737d52716ff74a0b563e72
alvin <ddrndr02d27d086h@studenti.unical.it>
2024-02-01 10:40:22 +0100

    commento prenotazione tavolo

--- FrontEnd/src/app/prenotazionetavolo/prenotazionetavolo.component.ts
+++ FrontEnd/src/app/prenotazionetavolo/prenotazionetavolo.component.ts
@@ -1,10 +1,17 @@
+// Importa la classe `Component` dal modulo '@angular/core'
 import { Component } from '@angular/core';
 
+// Definisce il componente Angular con il selettore 'app-prenotazionetavolo'
+// e specifica il file HTML e il file CSS associati a questo componente.
 @Component({
-  selector: 'app-prenotazionetavolo',
-  templateUrl: './prenotazionetavolo.component.html',
-  styleUrls: ['./prenotazionetavolo.component.css']
+  selector: 'app-prenotazionetavolo', // Selettore del componente nell'HTML
+  templateUrl: './prenotazionetavolo.component.html', // Percorso al file HTML del componente
+  styleUrls: ['./prenotazionetavolo.component.css'] // Percorso ai file CSS del componente
 })
-export class PrenotazionetavoloComponent {
 
+// Definisce la classe del componente chiamata PrenotazionetavoloComponent
+export class PrenotazionetavoloComponent {
+  // Questa classe è vuota al momento. Puoi aggiungere qui le proprietà e i metodi
+  // necessari per implementare la logica del tuo componente.
 }
+
