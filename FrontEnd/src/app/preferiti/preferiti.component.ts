a22a72e4ec110f0bbe31021f15fe332f487f0b85
alvin <ddrndr02d27d086h@studenti.unical.it>
2024-02-01 10:42:41 +0100

    commento preferiti

--- FrontEnd/src/app/preferiti/preferiti.component.ts
+++ FrontEnd/src/app/preferiti/preferiti.component.ts
@@ -1,11 +1,17 @@
+// Importa la classe `Component` dal modulo '@angular/core'
 import { Component } from '@angular/core';
 
+// Definisce il componente Angular con il selettore 'app-preferiti'
+// e specifica il file HTML e il file CSS associati a questo componente.
 @Component({
-  selector: 'app-preferiti',
-  templateUrl: './preferiti.component.html',
-  styleUrls: ['./preferiti.component.css']
+  selector: 'app-preferiti', // Selettore del componente nell'HTML
+  templateUrl: './preferiti.component.html', // Percorso al file HTML del componente
+  styleUrls: ['./preferiti.component.css'] // Percorso ai file CSS del componente
 })
-export class PreferitiComponent {
-  preferiti=9;// Proprietà del componente che rappresenta i preferiti (valore iniziale: 9)
 
+// Definisce la classe del componente chiamata PreferitiComponent
+export class PreferitiComponent {
+  // Proprietà del componente che rappresenta il numero di preferiti (valore iniziale: 9)
+  preferiti = 9;
 }
+
