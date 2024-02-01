6e775dc4ceee8afde6e70f38e88a96b937f050c2
Frumorn12 <andreabrescia002@gmail.com>
2024-02-01 10:35:13 +0100

    FIX HTML PRENOTAZIONE

--- FrontEnd/src/app/prenotazioneasporto/prenotazioneasporto.component.ts
+++ FrontEnd/src/app/prenotazioneasporto/prenotazioneasporto.component.ts
@@ -4,9 +4,9 @@ import { Component } from '@angular/core';
 // Definisce il componente Angular con il selettore 'app-prenotazioneasporto'
 // e specifica il file HTML e il file CSS associati a questo componente.
 @Component({
-  selector: 'app-prenotazioneasporto', // Selettore del componente nell'HTML
-  templateUrl: './prenotazioneasporto.component.html', // Percorso al file HTML del componente
-  styleUrls: ['./prenotazioneasporto.component.css'] // Percorso ai file CSS del componente
+  selector: 'app-prenotazioneasporto',
+  templateUrl: './prenotazioneasporto.component.html',
+  styleUrls: ['./prenotazioneasporto.component.css']
 })
 
 // Definisce la classe del componente chiamata PrenotazioneasportoComponent
