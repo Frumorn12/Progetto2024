67f532dd315638978a6d826b8aa6b95c1e8ca04e
Giada Francesca <rcegfr02r56e379u@studenti.unical.it>
2024-02-01 10:05:42 +0100

    commenti relativi al codice

--- FrontEnd/src/app/bevande/bevande.component.spec.ts
+++ FrontEnd/src/app/bevande/bevande.component.spec.ts
@@ -1,20 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BevandeComponent } from './bevande.component';
-
+// Dichiarazione delle variabili necessarie per i test
 describe('BevandeComponent', () => {
   let component: BevandeComponent;
   let fixture: ComponentFixture<BevandeComponent>;
-
+// Blocco che viene eseguito prima di ogni test
   beforeEach(() => {
+    // Configurazione del modulo di testing di Angular
     TestBed.configureTestingModule({
-      declarations: [BevandeComponent]
+      declarations: [BevandeComponent] // Dichiarazione del componente da testare
     });
+    // Creazione di un'istanza del componente utilizzando TestBed
     fixture = TestBed.createComponent(BevandeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
+// Singolo test che verifica se il componente può essere creato correttamente
   it('should create', () => {
     expect(component).toBeTruthy();
   });
