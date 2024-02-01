fc543df714ac5dd789d7c86b5d9c06b5891a761f
alvin <ddrndr02d27d086h@studenti.unical.it>
2024-02-01 10:31:35 +0100

    commento prenotazione asporto

--- FrontEnd/src/app/prenotazioneasporto/prenotazioneasporto.component.ts
+++ FrontEnd/src/app/prenotazioneasporto/prenotazioneasporto.component.ts
@@ -1,13 +1,17 @@
+// Importa la classe `Component` dal modulo '@angular/core'
 import { Component } from '@angular/core';
 
+// Definisce il componente Angular con il selettore 'app-prenotazioneasporto'
+// e specifica il file HTML e il file CSS associati a questo componente.
 @Component({
-  selector: 'app-prenotazioneasporto',
-  templateUrl: './prenotazioneasporto.component.html',
-  styleUrls: ['./prenotazioneasporto.component.css']
-
+  selector: 'app-prenotazioneasporto', // Selettore del componente nell'HTML
+  templateUrl: './prenotazioneasporto.component.html', // Percorso al file HTML del componente
+  styleUrls: ['./prenotazioneasporto.component.css'] // Percorso ai file CSS del componente
 })
+
+// Definisce la classe del componente chiamata PrenotazioneasportoComponent
 export class PrenotazioneasportoComponent {
-  antipasti=1;
-
-
+  // Dichiarazione di una variabile 'antipasti' inizializzata a 1
+  antipasti = 1;
 }
+
